b89e3c5617b955e3a71a11a7f9b00dceedeb2cb6
hoangndm <mhoang.9699@gmail.com>
2022-09-09 00:03:48 +0700

    config scss

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { override, useBabelRc } = require("customize-cra");
-const path = require("path");
 
 module.exports = override(useBabelRc());
